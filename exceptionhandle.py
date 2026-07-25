ff3351676c3837237255646f6752a5bc8c394fd2
Dhanalakshmi Panugandla <panugandladhanalakshmi2005@gmail.com>
2026-07-25 11:53:27 +0530

    Add exception handling examples in exceptionhandle.py

---
+++ exceptionhandle.py
@@ -0,0 +1,44 @@
+# try:
+#   print(1/0)
+# except:  
+#   print("zero division error")
+
+
+
+# try:
+#   x=int(input())
+#   print(x)
+# except ValueError as e:
+#   print(e)
+# except:
+#   print("error")
+
+# try:
+#   x=int(input())
+#   print(x)
+# except:
+#   print("error")
+# finally:
+#   print("done")
+
+# try:
+#   x=int(input())
+#   print(x)
+# except:
+#   print("error")
+# else:
+#   print("x is a number")
+
+# with open("file.txt","w") as f:
+#     f.write("hello world")
+
+# x=int(input())
+# if x<0:
+#   raise ValueError("number should be positive")
+
+# class dummy():
+#     def __init__(self,name):
+#         self.name=name
+# d=dummy("sriram")
+# print(d.name)
+    
