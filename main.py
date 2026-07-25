7534c546687c2eaa31d0f27ae176a5b64894977e
Dhanu-369 <panugandladhanalakshmi2005@gmail.com>
2026-07-25 11:39:39 +0530

    Add main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#n=int(input("enter a number:"))
+#if n%2==0 :
+#  print("even")
+#else :
+#print("odd")  
+
+
+
+# dict={
+#     "name":"sriram",
+#     "gender":"male",
+#     "age":20,
+#     "courses":["python","java","datascience"]
+
+# }
+# dict.update({"name":"dhana",
+#              "gender":"female"})
+# print(dict)
+# print(dict.keys())
+# print(dict.items())
+# print(dict.values())
+# print(dict["age"])
+# print(dict.get("name"))
+
+
+l=[1,2,3,4,5,3]
+l.remove(3)
+
